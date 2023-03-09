66abcb1a8c5141717db9ae3b489b9e8f4ced34e1
Sofia <dragsy13@bk.ru>
2023-03-09 18:57:03 +0300

    Add new task

---
+++ Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] InitMatrix()
+{
+    int[,] matrix = new int[3,4];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(1,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int FindAverage (int[,]matrix)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double average = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            average = (average + matrix[i, j]);
+        }
+        average = average / 3;
+    }
+}
+
+int[,] matrix = InitMatrix();
+PrintMatrix(matrix);
+int average = FindAverage (matrix);
+
+System.Console.WriteLine($"Среднее арифметическое каждого столбца: {average}");
